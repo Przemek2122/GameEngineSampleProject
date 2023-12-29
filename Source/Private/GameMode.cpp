047286c59a618702ec3c821168ae834c6e8c6a08
Przemek <przemek12wiewiora@gmail.com>
2023-12-29 23:53:00 +0100

    Add GameMode

---
+++ Source/Private/GameMode.cpp
@@ -0,0 +1,8 @@
+// Created by Przemys³aw Wiewióra 2020
+
+#include "CoreEngine.h"
+#include "GameMode.h"
+
+void FGameMode::StartGame()
+{
+}
---
+++ Source/Public/GameMode.h
@@ -0,0 +1,15 @@
+// Created by Przemys³aw Wiewióra 2020
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+class FGameMode
+{
+public:
+	FGameMode();
+
+	void StartGame();
+	void EndGame();
+	
+};
